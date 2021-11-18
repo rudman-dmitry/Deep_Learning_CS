1da628539b1153903c3384e9bd6fb43930de6b5d
Romy <romy.peled@campus.technion.ac.il>
2021-11-18 18:37:50 +0200

    updated

--- hw1/hw1/answers.py
+++ hw1/hw1/answers.py
@@ -10,9 +10,18 @@ math (delimited with $$).
 
 part1_q1 = r"""
 **Your answer:**
-1. **False.** Test set allows us to test the trained model and estimate it's ability of generalization.
-2. **False.** The split is affected by the representativeness of train and test set.
-3. **True.** The goal of cross-validation is to test the model's ability to predict new data that was not used in estimating it.
+1. **False.** The test set *does not* allow us to test our in-sample error. The in-sample error is by definition the
+average of the loss function over the training set samples. The test set allows us to test the trained model and estimate its ability to generalize.
+2. **False.** Different splits of the data into disjoint subsets may constitute training sets of different usefulness.
+For instance, in an extreme case, if all positive samples appear before the negative ones in the dataset, the training set might consist
+of only positive samples and bias the learning algorithm towards a positive classification. In other cases, the data might be
+organized in other ways that yield non-descriptive or non-representative training set. For this reason, selecting the training set 
+randomly from the data is more robust against artificial patterns.
+3. **True.** The goal of cross-validation is to test the model's ability to predict new data that was not seen in training time.
+The test set should not be included in the cross-validation process - using the test set during cross-validation may lead to
+data leakage - the learning algorithm is at risk of overfitting the test set which will render the test set a faulty indicator of 
+the learner ability to generalize. I.e., the chosen hyper-parameter values might be ones that overfit the test set and therefore achieve 
+excellent results for it, but in reality do not generalize well for the underlying distribution.
 4. **True.** It's not a perfect indicator of model generalizability simply because we DO see the model performance on the validation set, and so we subconsciously tune hyperparameters to better fit the validation set accuracy (or in the case of k-fold cross-validation, validation set(s). A better evaluation of a models generalizability is on a "test" set.
 
 
@@ -104,7 +113,7 @@ We can further see that certain areas are
 However, the linear model determines similarity to a class by creating a general "stencil" representation to it out of the input data, and uses the inner-product
  with it as the similarity score, 
 whereas the knn model uses all training data as representatives and determines the similarity to each class (roughly speaking) by the euclidean distance to its
- representatives. More accurately, the knn model determines which class a given sample is most similar to by checking to which class representatives it is the closest to. 
+ representatives. More accurately, the knn model determines which class a given sample is most similar to by checking which class representatives it is the closest to. 
 
  
 
@@ -114,7 +123,7 @@ part3_q3 = r"""
 **Your answer:**
 1. Based on the loss graph, the learning rate we chose is good, the loss declines exponentially and converges. Too low a learning rate would
  induce an overly slow learning, even when the loss function and its gradients assume high values. 
- In this case and we'd observe a much slower decline, and the loss would not converge within the given epochs.
+ In this case we'd observe a much slower decline, and the loss would not converge within the given epochs.
 Conversely, if the learning rate was too high, we would notice a jagged decline, or no decline at all. Too big a stride in the opposite direction of the gradient
  would throw us off the direction of a local\global minimum and go uphill instead of downhill. Too high a learning rate would also most likely
   not converge whatsoever since the big strides will miss local\global minima.
@@ -131,44 +140,33 @@ Overall we would say that the model constructs fairly simple (low resolution) re
 
 # ==============
 # Part 4 answers
-
 part4_q1 = r"""
-**Your answer:**
+*Your answer:*
 
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+The ideal pattern in a residual plot will be represented by the normally distributed y-hats across the x-axis. The indication of how our model fits the data we will get from the proximity of values to the zero line.
 
 """
 
 part4_q2 = r"""
-**Your answer:**
+*Your answer:*
 
+1. The model is linear. By adding nonlinear features we increase the capacity of a model to fit the data, which may be not linear.
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+2. When there is a non-linear relation between the target and the features, fitting the right non-linear function will result in better linear separability in new coordinate space. Such transformations doesn't help to any nonlinearity, only the ones following the speciffic distribution
 
+3. Adding non-linear features will not affect the decision boundary, it just will increase the separability.
 """
 
 part4_q3 = r"""
-**Your answer:**
+*Your answer:*
 
+1. We used np.logspace because values of 𝜆 are small, it increases their range.
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+2. N = k_folds * lambda_range * degree_range = 3 * 20 * 4 = 240
+
 
 """
-
 # ==============
+
+
