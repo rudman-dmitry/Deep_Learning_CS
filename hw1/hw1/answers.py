38e19fec59ec8c17a7a617093177776b31969ccc
rudman-dmitry <mr.dmitriyr@gmail.com>
2021-11-18 15:36:35 +0200

    first commit

---
+++ hw1/hw1/answers.py
@@ -0,0 +1,145 @@
+r"""
+Use this module to write your answers to the questions in the notebook.
+
+Note: Inside the answer strings you can use Markdown format and also LaTeX
+math (delimited with $$).
+"""
+
+# ==============
+# Part 1 answers
+
+part1_q1 = r"""
+**Your answer:**
+1. **False.** Test set allows us to test the trained model and estimate it's ability of generalization.
+2. **False.** The split is affected by the representativeness of train and test set.
+3. **True.** The goal of cross-validation is to test the model's ability to predict new data that was not used in estimating it.
+4. **True.** It's not a perfect indicator of model generalizability simply because we DO see the model performance on the validation set, and so we subconsciously tune hyperparameters to better fit the validation set accuracy (or in the case of k-fold cross-validation, validation set(s). A better evaluation of a models generalizability is on a "test" set.
+
+
+"""
+
+part1_q2 = r"""
+**Your answer:** 
+
+It's not a good approach. Now he just overfiiting regarding to the test set, he doesn't improve generalizabilty. It can result in minor improvement, but this approach is not justified.
+
+"""
+
+# ==============
+# Part 2 answers
+
+part2_q1 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part2_q2 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+# ==============
+
+# ==============
+# Part 3 answers
+
+part3_q1 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part3_q2 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part3_q3 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+# ==============
+
+# ==============
+# Part 4 answers
+
+part4_q1 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part4_q2 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part4_q3 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+# ==============
---
+++ hw1/hw1/linear_regression.py
@@ -0,0 +1,271 @@
+import numpy as np
+import sklearn
+from pandas import DataFrame
+from typing import List
+from sklearn.base import BaseEstimator, RegressorMixin, TransformerMixin
+from sklearn.utils import check_array
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.utils.validation import check_X_y, check_is_fitted
+
+
+class LinearRegressor(BaseEstimator, RegressorMixin):
+    """
+    Implements Linear Regression prediction and closed-form parameter fitting.
+    """
+
+    def __init__(self, reg_lambda=0.1):
+        self.reg_lambda = reg_lambda
+
+    def predict(self, X):
+        """
+        Predict the class of a batch of samples based on the current weights.
+        :param X: A tensor of shape (N,n_features_) where N is the batch size.
+        :return:
+            y_pred: np.ndarray of shape (N,) where each entry is the predicted
+                value of the corresponding sample.
+        """
+        X = check_array(X)
+        check_is_fitted(self, "weights_")
+
+        # TODO: Calculate the model prediction, y_pred
+
+        y_pred = None
+        # ====== YOUR CODE: ======
+        y_pred = np.matmul(self.weights_, X.T)
+        # ========================
+
+        return y_pred
+
+    def fit(self, X, y):
+        """
+        Fit optimal weights to data using closed form solution.
+        :param X: A tensor of shape (N,n_features_) where N is the batch size.
+        :param y: A tensor of shape (N,) where N is the batch size.
+        """
+        X, y = check_X_y(X, y)
+
+        # TODO:
+        #  Calculate the optimal weights using the closed-form solution you derived.
+        #  Use only numpy functions. Don't forget regularization!
+
+        w_opt = None
+        # ====== YOUR CODE: ======
+        n, d = X.shape
+        I = np.identity(d)
+        I[0][0] = 0
+        # Multiplications
+        reg = n*self.reg_lambda*I
+        X_XT = np.matmul(X.T,X)
+        XT_y = np.matmul(y.T,X)
+        # Inverse 
+        inverse = np.linalg.inv(X_XT+reg)
+        w_opt = np.matmul(inverse, XT_y)
+        # ========================
+
+        self.weights_ = w_opt
+        return self
+
+    def fit_predict(self, X, y):
+        return self.fit(X, y).predict(X)
+
+
+def fit_predict_dataframe(
+    model, df: DataFrame, target_name: str, feature_names: List[str] = None,
+):
+    """
+    Calculates model predictions on a dataframe, optionally with only a subset of
+    the features (columns).
+    :param model: An sklearn model. Must implement fit_predict().
+    :param df: A dataframe. Columns are assumed to be features. One of the columns
+        should be the target variable.
+    :param target_name: Name of target variable.
+    :param feature_names: Names of features to use. Can be None, in which case all
+        features are used.
+    :return: A vector of predictions, y_pred.
+    """
+    # TODO: Implement according to the docstring description.
+    # ====== YOUR CODE: ======
+    target = df[target_name]
+    df = df.drop(target_name, axis=1)
+    if feature_names:
+        df = df[feature_names]
+    y_pred = model.fit_predict(df, target)
+    # ========================
+    return y_pred
+
+
+class BiasTrickTransformer(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X: np.ndarray):
+        """
+        :param X: A tensor of shape (N,D) where N is the batch size and D is
+        the number of features.
+        :returns: A tensor xb of shape (N,D+1) where xb[:, 0] == 1
+        """
+        X = check_array(X, ensure_2d=True)
+
+        # TODO:
+        #  Add bias term to X as the first feature.
+        #  See np.hstack().
+
+        xb = None
+        # ====== YOUR CODE: ======
+        ones_col = np.ones((X.shape[0], 1))
+        xb = np.hstack([ones_col, X])
+        # ========================
+
+        return xb
+
+
+class BostonFeaturesTransformer(BaseEstimator, TransformerMixin):
+    """
+    Generates custom features for the Boston dataset.
+    """
+
+    def __init__(self, degree=2):
+        self.degree = degree
+
+        # TODO: Your custom initialization, if needed
+        # Add any hyperparameters you need and save them as above
+        # ====== YOUR CODE: ======
+        # raise NotImplementedError()
+        # ========================
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        """
+        Transform features to new features matrix.
+        :param X: Matrix of shape (n_samples, n_features_).
+        :returns: Matrix of shape (n_samples, n_output_features_).
+        """
+        X = check_array(X)
+
+        # TODO:
+        #  Transform the features of X into new features in X_transformed
+        #  Note: You CAN count on the order of features in the Boston dataset
+        #  (this class is "Boston-specific"). For example X[:,1] is the second
+        #  feature ('ZN').
+
+        X_transformed = None
+        # ====== YOUR CODE: ======
+        X_sq = X**2
+        X_cu = X**3
+        neg_exp = np.exp(-X)
+        neg_exp2 = np.exp(-X**2)
+        X_sqrt = np.sqrt(X)
+        X_f = X**5
+        X_cbrt = np.cbrt(X)
+        roll_1 = np.multiply(np.roll(X, 1, axis=1), X)
+        roll_2 = np.multiply(np.roll(X, 2, axis=1), X)
+        roll_3 = np.multiply(np.roll(X, 3, axis=1), X)
+        roll_5 = np.multiply(np.roll(X, 5, axis=1), X)
+        roll_7 = np.multiply(np.roll(X, 7, axis=1), X)
+        X_transformed = np.hstack((X, X_sq, X_cu, neg_exp, neg_exp2, X_sqrt, X_f, X_cbrt, roll_1, roll_2, roll_3, roll_5, roll_7))
+        
+        # ========================
+
+        return X_transformed
+
+
+def top_correlated_features(df: DataFrame, target_feature, n=5):
+    """
+    Returns the names of features most strongly correlated (correlation is
+    close to 1 or -1) with a target feature. Correlation is Pearson's-r sense.
+
+    :param df: A pandas dataframe.
+    :param target_feature: The name of the target feature.
+    :param n: Number of top features to return.
+    :return: A tuple of
+        - top_n_features: Sequence of the top feature names
+        - top_n_corr: Sequence of correlation coefficients of above features
+        Both the returned sequences should be sorted so that the best (most
+        correlated) feature is first.
+    """
+
+    # TODO: Calculate correlations with target and sort features by it
+
+    # ====== YOUR CODE: ======
+    # Getting a correlation matrix
+    corr_mat = df.corr(method='pearson')[target_feature]
+    # Dropping
+    corr_mat = corr_mat.drop(target_feature)
+    # Top features
+    top_n_corr = corr_mat.abs().sort_values(ascending=False)[:n]
+    top_n_features = list(top_n_corr.index)
+    top_n_corr = top_n_corr.values
+    # ========================
+
+    return top_n_features, top_n_corr
+
+
+def mse_score(y: np.ndarray, y_pred: np.ndarray):
+    """
+    Computes Mean Squared Error.
+    :param y: Predictions, shape (N,)
+    :param y_pred: Ground truth labels, shape (N,)
+    :return: MSE score.
+    """
+
+    # TODO: Implement MSE using numpy.
+    # ====== YOUR CODE: ======
+    mse = np.square(y-y_pred).mean()
+    # ========================
+    return mse
+
+
+def r2_score(y: np.ndarray, y_pred: np.ndarray):
+    """
+    Computes R^2 score,
+    :param y: Predictions, shape (N,)
+    :param y_pred: Ground truth labels, shape (N,)
+    :return: R^2 score.
+    """
+
+    # TODO: Implement R^2 using numpy.
+    # ====== YOUR CODE: ======
+    r2 = 1-np.sum(np.square(y-y_pred))/np.sum(np.square(y-y.mean()))
+    # ========================
+    return r2
+
+
+def cv_best_hyperparams(
+    model: BaseEstimator, X, y, k_folds, degree_range, lambda_range
+):
+    """
+    Cross-validate to find best hyperparameters with k-fold CV.
+    :param X: Training data.
+    :param y: Training targets.
+    :param model: sklearn model.
+    :param lambda_range: Range of values for the regularization hyperparam.
+    :param degree_range: Range of values for the degree hyperparam.
+    :param k_folds: Number of folds for splitting the training data into.
+    :return: A dict containing the best model parameters,
+        with some of the keys as returned by model.get_params()
+    """
+
+    # TODO: Do K-fold cross validation to find the best hyperparameters
+    #  Notes:
+    #  - You can implement it yourself or use the built in sklearn utilities
+    #    (recommended). See the docs for the sklearn.model_selection package
+    #    http://scikit-learn.org/stable/modules/classes.html#module-sklearn.model_selection
+    #  - If your model has more hyperparameters (not just lambda and degree)
+    #    you should add them to the search.
+    #  - Use get_params() on your model to see what hyperparameters is has
+    #    and their names. The parameters dict you return should use the same
+    #    names as keys.
+    #  - You can use MSE or R^2 as a score.
+
+    # ====== YOUR CODE: ======
+    hyper_p = {'linearregressor__reg_lambda': lambda_range,
+               'bostonfeaturestransformer__degree': degree_range}
+    search = sklearn.model_selection.GridSearchCV(model, param_grid=hyper_p, cv=k_folds)
+    search.fit(X, y)
+    best_params = search.best_params_
+    # ========================
+
+    return best_params
