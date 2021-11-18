910dea32fc06fad9c0cb15308b266b5a16ebbfb0
Romy <romy.peled@campus.technion.ac.il>
2021-11-18 16:37:09 +0200

    dry answers to part 2+3

--- hw1/hw1/answers.py
+++ hw1/hw1/answers.py
@@ -30,27 +30,33 @@ It's not a good approach. Now he just overfiiting regarding to the test set, he 
 
 part2_q1 = r"""
 **Your answer:**
+Increasing k does not necessarily lead to better generalization for unseen data, as can be seen
+in our cross-validation results. Indeed, if k is too low, the learning algorithm may be sensitive
+to noise in the data and overfit it, and therefore not generalize well. In this sense increasing 
+k up to a certain point leads to a better generalization capability. However, when k is too large, 
+the algorithm underfits the data and fails to generalize well - it begins to treat all points in the
+sampling space similarly, regardless of their location. At the extreme case where k=N, where 
+N is the number of samples in the training set, the algorithm classifies all points in the sampling space
+the same - as the mode class of the training set. 
 
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
 
 """
 
 part2_q2 = r"""
 **Your answer:**
+1. Training on the entire train-set with different models and choosing the model with the best
+accuracy on the train set will lead to an overfitting of the *train-set* and generalize poorly to unseen data. 
+2. Training on the entire train-set with various models and selecting the model with the best *test accuracy* may overfit
+ the *test-set* and not generalize well to unseen data.  
+ 
+ 
+ In both cases, K-fold cross validation avoids the problem of overfitting by estimating the generalization error and choosing
+  the models with the lowest estimated generalization error. The estimation of the generalization error is conducted on 
+  different validation sets that weren't seen by the algorithm at training time, and averaging over them. This renders the
+  estimation more accurate and less sensitive to noise in the training data.  
+ 
 
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
 
 """
 
@@ -61,41 +67,64 @@ An equation: $e^{i\pi} -1 = 0$
 
 part3_q1 = r"""
 **Your answer:**
+The value of $\Delta>0$ determines how much larger the score of a sample's true class should be in comparison
+to another class, in order to not be penalized.
 
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+I.e., $\Delta>0$ 's purpose is to ensure that a sample falls well "within"
+its ground-truth label hyperplane in comparison to the other classes' hyperplanes. 
+Different (numerically reasonable) values of $\Delta>0$ will yield equivalent optimization problems (equivalent loss functions)
+that only differ by their scaling.
 
 """
 
 part3_q2 = r"""
 **Your answer:**
+1. 
+For each class, we expect the learning algorithm to find weights which give high scores to samples from the class 
+(i.e., have a high inner-product with them) and low scores for samples from other classes (have a low inner-product with them).
+A way to achieve that is to have high-values where there should be high values(to reward correct samples), and low values where there shouldn't be high values
+(to penalize incorrect samples). That is why for a given entry(\pixel in the image)
+and a given class $j$, we expect $w_j$ to have a high value in this entry if it is likely to be black in a sample from class *j*, and low otherwise.
+This is supported by the visualization of the learned weights. For each digit - the weights are white where the digit should appear(where the entries in the input
+are expected to be white) and darker elsewhere. That is, the linear algorithm learns which pixels should be white and which should be black by establishing
+weights that are similar to the average input from each class.
+We can further see that certain areas are
+ particularly penalized - for example, the inner circle of the digit 0 - as opposed to others which can be both black or white.
+ 
+ Some classification errors arise from class properties that are not fully captured by such a representation, or properties/features whose representation
+  is too dependant upon location. For instance, we can see
+  that a 3 was misclassified as a 2, possibly because the 3 is slightly tilted. That is, the representation of 3 expects the three edges of the
+    "horizontal" parts to be lower and the middle one more inward. We also observe that the representations for 2 and 7 are rather similar,
+     and a 2 was misclassified as a 7, probably because the lower horizontal line of the 2 was a bit higher than captured by the corresponding weight
+      and was misclassified as a middle horizontal line of a 7. Here again we see over-sensitivity to the exact location of parts of digits, 
+      and also insufficient sensitivity to other properties, such as the curly upper line of a 2 as opposed to the (usually) straight upper line of a 7.
 
+2. Generally speaking, both models employ some kind of a similarity rule to determine which class an input sample belongs to. 
+However, the linear model determines similarity to a class by creating a general "stencil" representation to it out of the input data, and uses the inner-product
+ with it as the similarity score, 
+whereas the knn model uses all training data as representatives and determines the similarity to each class (roughly speaking) by the euclidean distance to its
+ representatives. More accurately, the knn model determines which class a given sample is most similar to by checking to which class representatives it is the closest to. 
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+ 
 
 """
 
 part3_q3 = r"""
 **Your answer:**
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
+1. Based on the loss graph, the learning rate we chose is good, the loss declines exponentially and converges. Too low a learning rate would
+ induce an overly slow learning, even when the loss function and its gradients assume high values. 
+ In this case and we'd observe a much slower decline, and the loss would not converge within the given epochs.
+Conversely, if the learning rate was too high, we would notice a jagged decline, or no decline at all. Too big a stride in the opposite direction of the gradient
+ would throw us off the direction of a local\global minimum and go uphill instead of downhill. Too high a learning rate would also most likely
+  not converge whatsoever since the big strides will miss local\global minima.
+  
+2. We would say that our model neither overfits nor underfits the training set. We observe that the train and validation curves grow together to a point of stability.
+If the model overfitted the training set the validation curve would begin to decrease from a certain point on.
+On the other hand, if the model underfitted the data, the training curve would be on the increase at the end of the learning process.  
+Overall we would say that the model constructs fairly simple (low resolution) representations for the classes that are not overly sensitive to noise\
+ \arbitrary features in the dataset and thus do not overfit it, but their simplicity also prevents the model from fully capturing different classes' subtleties. 
+ 
 """
 
 # ==============
