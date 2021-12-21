9c6977e0d6ddb545218c3b383bae649a8c5d71e6
rudman-dmitry <mr.dmitriyr@gmail.com>
2021-12-21 13:02:20 +0200

    part2 commit

---
+++ hw2/hw2/answers.py
@@ -0,0 +1,212 @@
+r"""
+Use this module to write your answers to the questions in the notebook.
+
+Note: Inside the answer strings you can use Markdown format and also LaTeX
+math (delimited with $$).
+"""
+
+# ==============
+# Part 1 answers
+
+
+def part1_arch_hp():
+    n_layers = 0  # number of layers (not including output)
+    hidden_dims = 0  # number of output dimensions for each hidden layer
+    activation = "none"  # activation function to apply after each hidden layer
+    out_activation = "none"  # activation function to apply at the output layer
+    # TODO: Tweak the MLP architecture hyperparameters.
+    # ====== YOUR CODE: ======
+    n_layers = 2
+    activation = "relu"
+    out_activation = "logsoftmax"
+    hidden_dims = 5
+    # ========================
+    return dict(
+        n_layers=n_layers,
+        hidden_dims=hidden_dims,
+        activation=activation,
+        out_activation=out_activation,
+    )
+
+
+def part1_optim_hp():
+    import torch.nn
+    import torch.nn.functional
+
+    loss_fn = None  # One of the torch.nn losses
+    lr, weight_decay, momentum = 0, 0, 0  # Arguments for SGD optimizer
+    # TODO:
+    #  - Tweak the Optimizer hyperparameters.
+    #  - Choose the appropriate loss function for your architecture.
+    #    What you returns needs to be a callable, so either an instance of one of the
+    #    Loss classes in torch.nn or one of the loss functions from torch.nn.functional.
+    # ====== YOUR CODE: ======
+    loss_fn = torch.nn.NLLLoss()
+    #loss_fn = torch.nn.MSELoss()
+    lr = 0.01
+    momentum = 0.5
+    weight_decay = 0.01
+    # ========================
+    return dict(lr=lr, weight_decay=weight_decay, momentum=momentum, loss_fn=loss_fn)
+
+
+part1_q1 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part1_q2 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part1_q3 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+
+part1_q4 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+# ==============
+# Part 2 answers
+
+
+def part2_optim_hp():
+    import torch.nn
+    import torch.nn.functional
+
+    loss_fn = None  # One of the torch.nn losses
+    lr, weight_decay, momentum = 0, 0, 0  # Arguments for SGD optimizer
+    # TODO:
+    #  - Tweak the Optimizer hyperparameters.
+    #  - Choose the appropriate loss function for your architecture.
+    #    What you returns needs to be a callable, so either an instance of one of the
+    #    Loss classes in torch.nn or one of the loss functions from torch.nn.functional.
+    # ====== YOUR CODE: ======
+    loss_fn = torch.nn.functional.cross_entropy
+    lr = 0.01
+    weight_decay = 0.0005
+    momentum = 0.8
+    # ========================
+    return dict(lr=lr, weight_decay=weight_decay, momentum=momentum, loss_fn=loss_fn)
+
+
+part2_q1 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+# ==============
+
+# ==============
+# Part 3 answers
+
+
+part3_q1 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part3_q2 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part3_q3 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part3_q4 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part3_q5 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+# ==============
